8fd3c47e25239864ebb6ae119d4e808b8d1c472f
binoooh <arvindacumos@gmail.com>
2025-06-02 22:27:19 +0800

    Day 52 Project CupCake App
    Day 52 HWS

---
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Vinz on 5/31/25.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
